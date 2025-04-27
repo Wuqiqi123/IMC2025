20b4ebca1087186f622f27ebf6c209b1f35394f1
wqq <1258552199@qq.com>
2025-04-27 22:34:24 +0800

    update

--- hloc/pairs_from_exhaustive.py
+++ hloc/pairs_from_exhaustive.py
@@ -1,36 +1,31 @@
 import argparse
 import collections.abc as collections
-import os
 from pathlib import Path
-from typing import Optional, Union, List
+from typing import List, Optional, Union
 
 from . import logger
-from .utils.parsers import parse_image_lists
 from .utils.io import list_h5_names
+from .utils.parsers import parse_image_lists
 
 
 def main(
-        output: Path,
-        image_list: Optional[Union[Path, List[str]]] = None,
-        features: Optional[Path] = None,
-        ref_list: Optional[Union[Path, List[str]]] = None,
-        ref_features: Optional[Path] = None):
-
+    output: Path,
+    image_list: Optional[Union[Path, List[str]]] = None,
+    features: Optional[Path] = None,
+    ref_list: Optional[Union[Path, List[str]]] = None,
+    ref_features: Optional[Path] = None,
+):
     if image_list is not None:
         if isinstance(image_list, (str, Path)):
-            if image_list.is_dir():
-                names_q = [x for x in os.listdir(str(image_list)) if x.endswith('.jpg') or x.endswith('.png')]
-                names_q.sort()
-            else:
-                names_q = parse_image_lists(image_list)
+            names_q = parse_image_lists(image_list)
         elif isinstance(image_list, collections.Iterable):
             names_q = list(image_list)
         else:
-            raise ValueError(f'Unknown type for image list: {image_list}')
+            raise ValueError(f"Unknown type for image list: {image_list}")
     elif features is not None:
         names_q = list_h5_names(features)
     else:
-        raise ValueError('Provide either a list of images or a feature file.')
+        raise ValueError("Provide either a list of images or a feature file.")
 
     self_matching = False
     if ref_list is not None:
@@ -39,8 +34,7 @@ def main(
         elif isinstance(image_list, collections.Iterable):
             names_ref = list(ref_list)
         else:
-            raise ValueError(
-                f'Unknown type for reference image list: {ref_list}')
+            raise ValueError(f"Unknown type for reference image list: {ref_list}")
     elif ref_features is not None:
         names_ref = list_h5_names(ref_features)
     else:
@@ -52,23 +46,19 @@ def main(
         for j, n2 in enumerate(names_ref):
             if self_matching and j <= i:
                 continue
-            # if j - i > 5:
-            #     continue
             pairs.append((n1, n2))
 
-    logger.info(f'Found {len(pairs)} pairs.')
-    with open(output, 'w') as f:
-        f.write('\n'.join(' '.join([i, j]) for i, j in pairs))
-    
-    return pairs
+    logger.info(f"Found {len(pairs)} pairs.")
+    with open(output, "w") as f:
+        f.write("\n".join(" ".join([i, j]) for i, j in pairs))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output', required=True, type=Path)
-    parser.add_argument('--image_list', type=Path)
-    parser.add_argument('--features', type=Path)
-    parser.add_argument('--ref_list', type=Path)
-    parser.add_argument('--ref_features', type=Path)
+    parser.add_argument("--output", required=True, type=Path)
+    parser.add_argument("--image_list", type=Path)
+    parser.add_argument("--features", type=Path)
+    parser.add_argument("--ref_list", type=Path)
+    parser.add_argument("--ref_features", type=Path)
     args = parser.parse_args()
     main(**args.__dict__)
